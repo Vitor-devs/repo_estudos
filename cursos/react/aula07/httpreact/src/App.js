3e09a7b67f9b399a955441b5aabaa2c5198d83ef
Vitor-devs <vitorcoelho2004@outlook.com>
2022-10-28 16:45:43 -0300

    usando api com react

--- cursos/react/aula07/httpreact/src/App.js
+++ cursos/react/aula07/httpreact/src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
-import {db} from '../data/db.json'
+import { useState, useEffect } from 'react';
 
-console.log(db);
+const url = 'http://localhost:3000/products';
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  //Resgatando dados
+  useEffect(() => {
+    async function fetchData(){
+      //Esperando a resposta da url
+      const response = await fetch(url);
+
+      //Transformando o texto em objeto 
+      const data = await response.json();
+
+      setProducts(data);
+    }
+    fetchData()
+  }, []); 
+  console.log(products);
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product)=>(
+          <div>
+            <li key={product.id}>Vendendo: {product.name} <br></br> Por apenas R$:{product.price} </li>
+            <br />
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
